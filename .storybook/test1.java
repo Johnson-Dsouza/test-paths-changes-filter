db33fc131dd7757d03bd5ff802d1c69de5217a43
Johnson Dsouza <johnsondsouza@codemancers.com>
2022-09-14 15:22:54 +0530

    Added java files

---
+++ .storybook/test1.java
@@ -0,0 +1,5 @@
+class testDemo1 {
+    public static void main(String[] args) {
+        System.out.println("Testing /.storybook path");
+    }
+}
---
+++ src/stories/test2.java
@@ -0,0 +1,5 @@
+class testDemo2 {
+    public static void main(String[] args) {
+        System.out.println("Testing src/stories path");
+    }
+}
