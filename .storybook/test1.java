6fbf18e515f25bf91aab28ea21b78a87a156dcb6
Johnson Dsouza <johnsondsouza@codemancers.com>
2022-09-14 15:54:15 +0530

    Updated action file

--- .storybook/test1.java
+++
@@ -1,5 +0,0 @@
-class testDemo1 {
-    public static void main(String[] args) {
-        System.out.println("Testing /.storybook path");
-    }
-}
--- src/stories/test2.java
+++
@@ -1,5 +0,0 @@
-class testDemo2 {
-    public static void main(String[] args) {
-        System.out.println("Testing src/stories path");
-    }
-}
